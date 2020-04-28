370f3ec001a5b03fe27f6bd27f6b413d0f227443
davidmalcat <david>
2020-04-28 13:42:25 +0200

    Core style gulp task created, helper and mixin files added.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,26 +4,36 @@ import gulp from 'gulp';
 import sass from 'gulp-sass';
 import babel from 'gulp-babel';
 import cleanCSS from 'gulp-clean-css';
+import sourcemaps from 'gulp-sourcemaps';
+import concat from 'gulp-concat';
+import uglify from 'gulp-uglify-es';
 import rename from 'gulp-rename';
 import del from 'del';
 
-const assetsPath = 'www/assets';
+sass.compiler = require('node-sass');
+
+const assetsPath = './www/assets';
 
 const paths = {
 	styles: {
+		vendor: {
+			main: assetsPath + '/scss/vendor/_main.scss',
+			src: assetsPath + '/scss/vendor/**/*.scss',
+			dest: assetsPath + '/dist/css/'
+		},
 		document: {
 			main: assetsPath + '/scss/document/_main.scss',
 			src: assetsPath + '/scss/document/**/*.scss',
 			dest: assetsPath + '/dist/css/'
 		},
+		core: {
+			main: assetsPath + '/scss/core/main.scss',
+			src: assetsPath + '/scss/core/**/*.scss',
+			dest: assetsPath + '/dist/css/'
+		},
 		layout: {
 			main: assetsPath + '/scss/layout/_main.scss',
 			src: assetsPath + '/scss/layout/**/*.scss',
-			dest: assetsPath + '/dist/css/'
-		},
-		core: {
-			main: assetsPath + '/scss/core/_main.scss',
-			src: assetsPath + '/scss/core/**/*.scss',
 			dest: assetsPath + '/dist/css/'
 		},
 		components: {
@@ -35,15 +45,36 @@ const paths = {
 			main: assetsPath + '/scss/ui/_main.scss',
 			src: assetsPath + '/scss/ui/**/*.scss',
 			dest: assetsPath + '/dist/css/'
-		},
-		vendor: {
-			main: assetsPath + '/scss/vendor/_main.scss',
-			src: assetsPath + '/scss/vendor/**/*.scss',
-			dest: assetsPath + '/dist/css/'
-		},
+		}
 	},
 	scripts: {
 		src: assetsPath + '/js/**/*.js',
 		dest: assetsPath + '/dist/js/'
 	}
 };
+
+export const clean = () => del([ assetsPath + '/dist' ]);
+
+export function coreStyles() {
+	return gulp.src(paths.styles.core.main)
+		.pipe(sass())
+		.pipe(sourcemaps.init())
+			.pipe(sourcemaps.identityMap())
+			.pipe(sourcemaps.write(''))
+		.pipe(cleanCSS())
+		.pipe(rename({
+			basename: '4-core',
+			suffix: '.min'
+		}))
+		.pipe(gulp.dest(paths.styles.core.dest));
+}
+
+function watchFiles() {
+	gulp.watch(paths.styles.core.src, coreStyles);
+}
+
+export { watchFiles as watch };
+
+const build = gulp.series(clean, gulp.parallel(coreStyles));
+
+export default build;
