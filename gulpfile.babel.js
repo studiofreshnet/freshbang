4890522455777f4fd666f7e112b9a26d651466cc
davidmalcat <david>
2020-04-29 14:27:38 +0200

    Vendor CSS gulp task, normalize files for cross-browser compatibility, dummy data.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -17,7 +17,7 @@ const assetsPath = './www/assets';
 const paths = {
 	styles: {
 		vendor: {
-			main: assetsPath + '/scss/vendor/_main.scss',
+			main: assetsPath + '/scss/vendor/main.scss',
 			src: assetsPath + '/scss/vendor/**/*.scss',
 			dest: assetsPath + '/dist/css/'
 		},
@@ -31,18 +31,13 @@ const paths = {
 			src: assetsPath + '/scss/core/**/*.scss',
 			dest: assetsPath + '/dist/css/'
 		},
-		layout: {
-			main: assetsPath + '/scss/layout/_main.scss',
-			src: assetsPath + '/scss/layout/**/*.scss',
-			dest: assetsPath + '/dist/css/'
-		},
 		components: {
-			main: assetsPath + '/scss/components/_main.scss',
+			main: assetsPath + '/scss/components/main.scss',
 			src: assetsPath + '/scss/components/**/*.scss',
 			dest: assetsPath + '/dist/css/'
 		},
 		ui: {
-			main: assetsPath + '/scss/ui/_main.scss',
+			main: assetsPath + '/scss/ui/main.scss',
 			src: assetsPath + '/scss/ui/**/*.scss',
 			dest: assetsPath + '/dist/css/'
 		}
@@ -54,6 +49,17 @@ const paths = {
 };
 
 export const clean = () => del([ assetsPath + '/dist' ]);
+
+export function vendorStyles() {
+	return gulp.src(paths.styles.vendor.main)
+		.pipe(sass())
+		.pipe(cleanCSS())
+		.pipe(rename({
+			basename: '1-vendor',
+			suffix: '.min'
+		}))
+		.pipe(gulp.dest(paths.styles.vendor.dest));
+}
 
 export function documentStyles() {
 	return gulp.src(paths.styles.document.main)
@@ -84,12 +90,13 @@ export function coreStyles() {
 }
 
 function watchFiles() {
-	gulp.watch(paths.styles.core.src, documentStyles);
+	gulp.watch(paths.styles.vendor.src, vendorStyles);
+	gulp.watch(paths.styles.document.src, documentStyles);
 	gulp.watch(paths.styles.core.src, coreStyles);
 }
 
 export { watchFiles as watch };
 
-const build = gulp.series(clean, gulp.parallel(documentStyles, coreStyles));
+const build = gulp.series(clean, gulp.parallel(vendorStyles, documentStyles, coreStyles));
 
 export default build;
